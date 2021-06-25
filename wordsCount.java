05fd2810b7e49e3028466b8d0e9c121ca12d9f95
Abdulrahman Mendahawi <67026595+Amendahawi@users.noreply.github.com>
2021-06-25 01:06:27 -0700

    Update wordsCount.java

--- wordsCount.java
+++ wordsCount.java
@@ -1,10 +1,26 @@
+/*Credit to CodingBat.com
+https://codingbat.com/prob/p124620
+
+Question: AP-1 > wordsCount
+Given an array of strings, return the count of the number of strings with the given length.
+
+wordsCount(["a", "bb", "b", "ccc"], 1) → 2
+wordsCount(["a", "bb", "b", "ccc"], 3) → 1
+wordsCount(["a", "bb", "b", "ccc"], 4) → 0
+
+Written by Abdulrahman Mendahawi.*/
 
 public int wordsCount(String[] words, int len) {
+  //Create a integer called counter so we can keep track of the number of strings with the given length.
   int counter = 0;
+  //Create a for loop so that we can loop through all the integers in the array.
   for (int i = 0; i < words.length; i++){
+    //If statement to check if the length of the item is the same as the integer len.
     if(words[i].length() == len){
+      //If valid (length is same as len), then we add 1 to counter.
       counter++;
     }
   }
+  //Return the counter.
   return counter;
 }
